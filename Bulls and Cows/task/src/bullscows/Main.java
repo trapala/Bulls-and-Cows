06f3b8fd97bf3369181c5e2ce5f340973357b034
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2022-12-22 18:57:59 +0100

    Initial commit

---
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -0,0 +1,40 @@
+package bullscows;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+         * Stage 1
+         */
+        System.out.println("""
+                The secret code is prepared: ****.
+                                
+                Turn 1. Answer:
+                1234
+                Grade: 1 cow.
+                                
+                Turn 2. Answer:
+                5678
+                Grade: 1 cow.
+                                
+                Turn 3. Answer:
+                9012
+                Grade: 1 bull and 1 cow.
+                                
+                Turn 4. Answer:
+                9087
+                Grade: 1 bull and 1 cow.
+                                
+                Turn 5. Answer:
+                1087
+                Grade: 1 cow.
+                                
+                Turn 6. Answer:
+                9205
+                Grade: 3 bulls.
+                                
+                Turn 7. Answer:
+                9305
+                Grade: 4 bulls.
+                Congrats! The secret code is 9305.""");
+    }
+}
