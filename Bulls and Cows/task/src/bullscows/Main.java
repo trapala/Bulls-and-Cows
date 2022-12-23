fdc814cdaac0dc6c2ccf413f43a9f586c8bcfb0d
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2022-12-23 21:51:57 +0100

    stage 3

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -3,22 +3,33 @@ package bullscows;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         /*
-         * Stage 2
+         * Stage 3
          */
         Scanner scanner = new Scanner(System.in);
-        String line = scanner.nextLine();
+        //  String line = scanner.nextLine();
+        int amountOfSecretNumber = scanner.nextInt();
+        StringBuilder sb = new StringBuilder();
 
-        String secretCode = "9305";
-        String[] secretCodeArr = secretCode.split("");
+        UniqueRng rng = new UniqueRng(amountOfSecretNumber);
+        while (rng.hasNext()) {
+            sb.append(rng.next());
+        }
 
-        int countCow = 0;
+        if (amountOfSecretNumber > 10) {
+            System.out.printf("Error: can't generate a secret number with a length of %d because there aren't enough unique digits.", amountOfSecretNumber);
+        } else {
+            System.out.printf("The random secret number is: %s", sb);
+        }
+
+/*        int countCow = 0;
         int countBull = 0;
 
-        for (int i = 0; i < secretCodeArr.length; i++) {
+        for (int i = 0; i < sb.length(); i++) {
             for (int j = 0; j < line.toCharArray().length; j++) {
-                if (secretCodeArr[i].matches(String.valueOf(line.charAt(j)))) {
+                if (sb.charAt(i) == line.charAt(j)) {
                     countCow++;
                     if (i == j) {
                         countBull++;
@@ -29,12 +40,11 @@ public class Main {
         }
 
         if (countBull == 0 && countCow == 0) {
-            System.out.printf("Grade: None. The secret code is %s.", secretCode);
+            System.out.printf("Grade: None. The secret code is %s.", sb);
         } else if (countCow != 0 && countBull == 0) {
-            System.out.printf("Grade: %d cow(s). The secret code is %s.", countCow, secretCode);
+            System.out.printf("Grade: %d cow(s). The secret code is %s.", countCow, sb);
         } else {
-            System.out.printf("Grade: %d bull(s) and %d cow(s). The secret code is %s.", countBull, countCow, secretCode);
-        }
-
+            System.out.printf("Grade: %d bull(s) and %d cow(s). The secret code is %s.", countBull, countCow, sb);
+        }*/
     }
 }
