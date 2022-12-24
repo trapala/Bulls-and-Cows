bff93db99a68b6070ce896c343a39aa11cfb47c9
DESKTOP-FMU69TA\frikon <rapala.tomek@gmail.com>
2022-12-24 11:55:29 +0100

    stage 4

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -6,12 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
         /*
-         * Stage 3
+         * Stage 4
          */
         Scanner scanner = new Scanner(System.in);
-        //  String line = scanner.nextLine();
-        int amountOfSecretNumber = scanner.nextInt();
         StringBuilder sb = new StringBuilder();
+        int countTurns = 1;
+        boolean flagEnd = false;
+
+        System.out.println("Please, enter the secret code's length:");
+        int amountOfSecretNumber = scanner.nextInt();
 
         UniqueRng rng = new UniqueRng(amountOfSecretNumber);
         while (rng.hasNext()) {
@@ -19,32 +22,45 @@ public class Main {
         }
 
         if (amountOfSecretNumber > 10) {
-            System.out.printf("Error: can't generate a secret number with a length of %d because there aren't enough unique digits.", amountOfSecretNumber);
-        } else {
-            System.out.printf("The random secret number is: %s", sb);
-        }
+            System.out.printf("Error: can't generate a secret number with a length of %d because there aren't enough unique digits.\n", amountOfSecretNumber);
+        } /*else {
+            System.out.printf("The random secret number is: %s\n", sb);
+        }*/
 
-/*        int countCow = 0;
-        int countBull = 0;
+        System.out.println("Okay, let's start a game!");
 
-        for (int i = 0; i < sb.length(); i++) {
-            for (int j = 0; j < line.toCharArray().length; j++) {
-                if (sb.charAt(i) == line.charAt(j)) {
-                    countCow++;
-                    if (i == j) {
-                        countBull++;
-                        countCow--;
+        while (!flagEnd) {
+            int countCows = 0;
+            int countBulls = 0;
+            System.out.println("Turn " + countTurns++ + ":");
+            String line = scanner.next();
+
+            for (int i = 0; i < sb.length(); i++) {
+                for (int j = 0; j < line.toCharArray().length; j++) {
+                    if (sb.charAt(i) == line.charAt(j)) {
+                        countCows++;
+                        if (i == j) {
+                            countBulls++;
+                            countCows--;
+                        }
                     }
                 }
             }
-        }
 
-        if (countBull == 0 && countCow == 0) {
-            System.out.printf("Grade: None. The secret code is %s.", sb);
-        } else if (countCow != 0 && countBull == 0) {
-            System.out.printf("Grade: %d cow(s). The secret code is %s.", countCow, sb);
-        } else {
-            System.out.printf("Grade: %d bull(s) and %d cow(s). The secret code is %s.", countBull, countCow, sb);
-        }*/
+            String bull = countBulls > 1 ? "bulls" : "bull";
+            String cow = countCows > 1 ? "cows" : "cow";
+
+            if (countBulls == 0 && countCows == 0) {
+                System.out.println("Grade: None.\n");
+            } else if (countCows != 0 && countBulls == 0) {
+                System.out.printf("Grade: %d %s.\n", countCows, cow);
+            } else if (countBulls == amountOfSecretNumber) {
+                System.out.printf("Grade: %d bulls\n", countBulls);
+                System.out.println("Congratulations! You guessed the secret code.");
+                flagEnd = true;
+            } else {
+                System.out.printf("Grade: %d %s and %d %s.\n", countBulls, bull, countCows, cow);
+            }
+        }
     }
 }
